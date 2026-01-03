2254900fbf40f732da08e48d6f2491311b9ba5b8
2k23csai2313841-dot <2k23.csai2313841@gmail.com>
2026-01-03 18:20:37 +0530

    Update checkTasks.js

--- api/checkTasks.js
+++ api/checkTasks.js
@@ -57,10 +57,10 @@ const minutesLeft = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
         <p><strong> Yeh le dekh aaj ke yeh sab task nahi hue</strong></p>
         <ul>${taskListHTML}</ul>
 
-        <br/>
+        
         <p>⏳ <strong>saale din khatam hone me sirf :</strong> ${hoursLeft} hours ${minutesLeft}   minutes <b> bacha hai kab karega din bar sota hai uth aur kar mote saale </b> </p>
 
-        <br/>
+        
         <a href="https://anubhav-task.vercel.app/"> jaa kar pura aur phir update kar ish par jaldi </a>
 
       `
@@ -73,6 +73,7 @@ const minutesLeft = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
     return res.status(500).json({ error: err.message });
   }
 }
+
 
 
 
